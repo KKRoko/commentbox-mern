175a485917027873769b9e5adcfe75759ed8bcfa
Konrad Kale <konrad.kale@icloud.com>
2017-02-07 23:24:57 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import CommentBox from './CommentBox';
+import './style';
 
 ReactDOM.render(
-  <App />,
+  <CommentBox />,
   document.getElementById('root')
 );
